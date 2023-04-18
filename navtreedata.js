10dd8165eb48fa782fb8bf952b4daf6914b9974a
Luis A Garcia <67930113+LugaGarcia@users.noreply.github.com>
2023-04-18 03:08:43 -0400

    First portion of uploading website

---
+++ navtreedata.js
@@ -0,0 +1,88 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Compact Game Engine", "index.html", [
+    [ "Add any additional notes here", "md__engine__r_e_a_d_m_e.html", [
+      [ "Game/Engine Publicity", "md__engine__r_e_a_d_m_e.html#autotoc_md1", null ],
+      [ "Compilation Instructions", "md__engine__r_e_a_d_m_e.html#autotoc_md2", null ],
+      [ "Project Hieararchy", "md__engine__r_e_a_d_m_e.html#autotoc_md3", [
+        [ "./Engine Directory Organization", "md__engine__r_e_a_d_m_e.html#autotoc_md4", null ]
+      ] ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", "globals_type" ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_audio_8cpp.html",
+"classexplosion_1_1_explosion_particle.html#a9ac7743882dbdf47bdbf211b33611a7f",
+"glad_8h.html#a0da0630e92d17a649f7209148fe249af",
+"glad_8h.html#a2497c5831528b8d1a419dfb409ba276d",
+"glad_8h.html#a3c3c26c15768b850c1b82ab4a5d77245",
+"glad_8h.html#a5155c381bb0f1f2d9d392e0440eb983b",
+"glad_8h.html#a66df9083d62016fa3bb45bec6f4f48e7",
+"glad_8h.html#a7c93daeeb5df73c848ded9bd15d5c31f",
+"glad_8h.html#a908891f3c7cc2d84decc363757186c69",
+"glad_8h.html#aa68335e779479b2d957ccc7d755d9e01",
+"glad_8h.html#abbc65ca04170d8ac3200dc8c5be26946",
+"glad_8h.html#ad04f223f50b2da67ee2f9fae646e619a",
+"glad_8h.html#ae729d6f714f0d94a71c83147bf0f0020",
+"glad_8h.html#afbb59b10257219344de62f02f5ac8150",
+"namespaceplayer.html#af7a374353d5f81d59f36135fd43f3584"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
