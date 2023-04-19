75be842dcd70f650f89cd04d8777b60a4f96c644
Luis A Garcia <67930113+LugaGarcia@users.noreply.github.com>
2023-04-19 03:29:30 -0400

    Updating website to new documentation pt2
    For those coming from Game Class:
    These changes are being done before the technical due date of 7 am on 4/19/23 due to it being due the 18 "anywhere on the world", meaning it is due at 7am the following day. This is followed by Piazza post @110

--- enemy__bullet_8py.js
+++ enemy__bullet_8py.js
@@ -6,6 +6,8 @@ var enemy__bullet_8py =
     [ "EN_BULLET_BBOX_W", "enemy__bullet_8py.html#a96a37455fc4e32e7a6fef6ee0f26a2cc", null ],
     [ "EN_BULLET_BBOX_X", "enemy__bullet_8py.html#ab007548786e49a1f2f4ad460d2861314", null ],
     [ "EN_BULLET_BBOX_Y", "enemy__bullet_8py.html#a236ba64d8474cb46ea8eb7a3e081cf14", null ],
+    [ "EN_BULLET_SPR_H", "enemy__bullet_8py.html#a3cc30aaaec37ef0530a3c4c57f23dc55", null ],
+    [ "EN_BULLET_SPR_W", "enemy__bullet_8py.html#a120d7fb324a7b67c12a3a6ec91484c93", null ],
     [ "EN_BULLET_SPR_X", "enemy__bullet_8py.html#a9e7fbab496b92129d52dfcf16c25e150", null ],
     [ "EN_BULLET_SPR_Y", "enemy__bullet_8py.html#a3428bf40bd5bdcdaa8f3976e4d4ffb13", null ]
 ];
