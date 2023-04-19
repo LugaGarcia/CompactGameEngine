9d832ea5b23905f32725a3dee9914c22d55e8e7c
Luis A Garcia <67930113+LugaGarcia@users.noreply.github.com>
2023-04-19 03:27:22 -0400

    Updating website to new documentation pt1
    For those coming from the Game Class:
    These changes are being done before the technical due date of 7 am on 4/19/23 due to it being due the 18 "anywhere on the world", meaning it is due at 7am the following day. This is followed by Piazza post @110

--- classenemy__bullet_1_1_enemy_bullet.js
+++ classenemy__bullet_1_1_enemy_bullet.js
@@ -1,6 +1,6 @@
 var classenemy__bullet_1_1_enemy_bullet =
 [
-    [ "__init__", "classenemy__bullet_1_1_enemy_bullet.html#a10affec353c03295bb152dbd38a37def", null ],
+    [ "__init__", "classenemy__bullet_1_1_enemy_bullet.html#a2905eae19cae4a34565100514bc13563", null ],
     [ "destroy", "classenemy__bullet_1_1_enemy_bullet.html#a88af42230cc0e1e07a3b87a086fe85d4", null ],
     [ "update", "classenemy__bullet_1_1_enemy_bullet.html#a5b02d567b364a8ba1f0b03438b1669f6", null ],
     [ "dmg", "classenemy__bullet_1_1_enemy_bullet.html#a0cf841ee0b5f05758a99f59e46d20baf", null ],
